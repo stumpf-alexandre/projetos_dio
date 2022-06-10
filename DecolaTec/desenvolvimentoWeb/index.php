1a93bfd26f3c90707edf01581a49e6f5c60924b9
Alexandre Stumpf <als.stumpf@gmail.com>
2022-06-09 23:12:08 -0300

    curso dio HTML e CSS

---
+++ DecolaTec/desenvolvimentoWeb/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Meu primeiro site em PHP pela DIO! Woohoo!</title>
+</head>
+<body>
+
+	<?php
+		for ($i= 0; $i < 10; $i++) { 
+			print("Linha número " . $i . "<br />");
+		}	
+	?>
+
+</body>
+</html>
