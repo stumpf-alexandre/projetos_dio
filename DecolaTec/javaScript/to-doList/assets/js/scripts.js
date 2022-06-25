dfb064dd9ff77f99255a117de579efa11fd1404c
Alexandre Stumpf <als.stumpf@gmail.com>
2022-06-25 20:46:12 -0300

    estilizando botão de deletar, mas não fazendo funcionar!

--- DecolaTec/javaScript/to-doList/assets/js/scripts.js
+++ DecolaTec/javaScript/to-doList/assets/js/scripts.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('task-form');
 const taskList = document.getElementById('tasks');
+var itemId;
 
 form.onsubmit = function (e) {
 	e.preventDefault();
@@ -14,20 +15,38 @@ form.onsubmit = function (e) {
 
 function addTask(description) {
 	const taskContainer = document.createElement('div');
-	const newTask = document.createElement('input');
+	const newTask1 = document.createElement('input');
 	const taskLabel = document.createElement('label');
+	const newTask2 = document.createElement('input');
 	const taskDescriptionNode = document.createTextNode(description);
+	itemId = taskList.childElementCount;
 
-	newTask.setAttribute('type', 'checkbox');
-	newTask.setAttribute('name', description);
-	newTask.setAttribute('id', description);
+	newTask1.setAttribute('type', 'checkbox');
+	newTask1.setAttribute('name', description);
+	newTask1.setAttribute('id', 'taskCheckbox');
+
+	newTask2.setAttribute('type', 'button');
+	newTask2.setAttribute('value', 'X');
+	newTask2.setAttribute('onclick', 'removeTask(' + itemId + ')');
+	newTask2.setAttribute('id','taskButton');
 
 	taskLabel.setAttribute('for', description);
+	taskLabel.setAttribute('id', 'label');
 	taskLabel.appendChild(taskDescriptionNode);
 
 	taskContainer.classList.add('task-item');
-	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(newTask1);
 	taskContainer.appendChild(taskLabel);
+	taskContainer.appendChild(newTask2);
+	
 
 	taskList.appendChild(taskContainer);
+}
+
+function removeTask(itemId){
+	for(i = 0; i < taskList.children.length; i++){
+		if(taskList.children[i].getAtribute('taskButton') == itemId){
+			taskList.children[i].remove();
+		}
+	}
 }
