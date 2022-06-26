09a327fd0a24c45fd2209ffe1d46bae12c9f8934
Alexandre Stumpf <als.stumpf@gmail.com>
2022-06-26 20:22:41 -0300

    Funções e suas particularidades

---
+++ DecolaTec/javaScript/aula5/script.js
@@ -0,0 +1,15 @@
+//declarando uma função
+function funcao(){
+    console.log('Tudo certo jovem!');
+}
+
+//chamando uma função
+funcao();
+
+//funções com parametros
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo);
+}
+
+//chamando a função
+mensagem('Tudo certo', 'jovem!');
