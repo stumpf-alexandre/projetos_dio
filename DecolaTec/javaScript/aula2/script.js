da6fce3c6ce128ec3df773caa58408550def4eaf
Alexandre Stumpf <als.stumpf@gmail.com>
2022-06-26 19:23:25 -0300

    entendendo variáveis e seus valores

---
+++ DecolaTec/javaScript/aula2/script.js
@@ -0,0 +1,80 @@
+//tipos primitivos
+//booleano
+var vOuF = false;
+console.log(typeof(vOuF));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = 'Diana';
+console.log(typeof(nome));
+
+//como declarar
+var variavel = 'Diana';
+variavel = 'Martine';
+console.log(typeof(variavel));
+
+let variavel2 = 'Diana';
+variavel2 = 'Martine'
+console.log(typeof(variavel2));
+
+const variavel3 = 'Diana';
+//variavel3 = 'Martine';
+//neste caso a variavel const não pode receber um outro dado pq ela deve receber um dado na sua criação e não recebe mais nem um
+console.log(typeof(variavel3));
+
+//adição
+var adicao = 1 + 1;
+console.log(adicao);
+
+//subtração
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+//multiplicação
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+//divisão real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+//divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+//potenciação
+var potenciacao = 2 ** 10
+console.log(potenciacao);
+
+//operador relacional
+//maior que
+var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+//menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual
+var maiorIgual = 5 >= 2;
+console.log(maiorIgual);
+
+//menor ou igual
+var menorIgual = 5 <= 2;
+console.log(menorIgual);
+
+//operadores lógicos
+//&&
+var e = true && false;
+console.log(e);
+
+//||
+var ou = true || false;
+console.log(ou);
+
+//!
+var nao = !true;
+console.log(nao);
