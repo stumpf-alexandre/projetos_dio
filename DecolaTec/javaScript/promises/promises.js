265a7d7852f7630bddf6bf40eee8e752a9dcef95
Alexandre Stumpf <als.stumpf@gmail.com>
2022-07-03 17:55:47 -0300

    quando a promise é resolvida voce consegue utilizala para fazer outras operações com await

--- DecolaTec/javaScript/promises/promises.js
+++ DecolaTec/javaScript/promises/promises.js
@@ -2,4 +2,11 @@ const myPromises = new Promise((resolve, reject){
     window.setTimeout(() => {
         resolve(console.log('Resolvida'));
     }, 2000);
+    //apos 2 segundos , retornara o valor
 });
+
+await myPromises
+        .then((result) => result + 'passando pelo then')
+        .then((result) => result + 'e agora acabou!')
+        .catch((err) => console.log(err.message));
+//resolvida passando pelo then e agora acabou!
