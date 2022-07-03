435d4497e4f120081c9dc653efc46ac9e99f8a64
Alexandre Stumpf <als.stumpf@gmail.com>
2022-07-03 20:00:53 -0300

    utilizando try/catch com async/await e promises

--- DecolaTec/javaScript/asyncAwait/asyncAwait.js
+++ DecolaTec/javaScript/asyncAwait/asyncAwait.js
@@ -13,3 +13,24 @@ async function resolvePromise() {
     return resolved;
 }
 //sem o async e o await a função promise não sera executada pq no javascript as funções são sincronas
+
+
+//utilizando try/catch com async/await e promises
+async function resolvePromise(){
+    const myPromise = new Promise((resolve, reject) => {
+        window.setTimeout(() => {
+            resolve('Resolvida');
+        }, 4000);
+    });
+
+    let result;
+
+    try {
+        result = await myPromise
+            .then((result) => result + 'passando pelo then')
+            .then((result) => result + 'e agora acabou!')
+    } catch (err) {
+        result = err.message;        
+    }
+    return result;
+}
