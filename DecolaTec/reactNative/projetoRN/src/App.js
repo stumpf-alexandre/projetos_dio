41574db321f0db5814c5c164c9089697dca6fead
Alexandre Stumpf <als.stumpf@gmail.com>
2022-07-14 23:22:34 -0300

    criando um safeAreaView e estilizando um statusBar

--- DecolaTec/reactNative/projetoRN/src/App.js
+++ DecolaTec/reactNative/projetoRN/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, StatusBar} from 'react-native';
 
 const App = () => {
     return (
-        <View>
-            <Text>Ola mundo!!!!</Text>
-        </View>
+        //protege os componentes para não sairem para locais indevidos da tela
+        <SafeAreaView>
+            <StatusBar backgroundColor = {'pink'} barStyle = "dark-content"/>
+
+            <View style = {style.container}>
+                <Text style = {style.text}>Ola mundo!!!!</Text>
+            </View>
+        </SafeAreaView>
     );
 };
 
 export default App;
+
+//criando um style para estilizar a view e o texto
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: 'pink',
+        flex: 1, //expande a view na tela inteira
+    },
+    text: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+});
