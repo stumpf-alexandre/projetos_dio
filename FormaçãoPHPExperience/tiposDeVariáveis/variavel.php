2c04d71317875ce9d20969477d1d0f23e712855d
stumpf-alexandre <als.stumpf@gmail.com>
2022-08-14 00:02:25 -0300

    declarando variaveis no php

---
+++ FormaçãoPHPExperience/tiposDeVariáveis/variavel.php
@@ -0,0 +1,16 @@
+<?php
+    //entregador de deliveri ganha 80 reais por dia porém ainda tem as gorjetas dos clientes
+    //declarando as rariaveis no php
+    $valorDia = 80;
+    $gorjetas = 40;
+    $diaria = $valorDia + $gorjetas;
+
+    echo "Segunda-feira : $diaria\n";
+
+    $gorjetas = 15;
+    $diaria = $valorDia + $gorjetas;
+
+    echo "Terça-feira : $diaria";
+
+    
+?>
