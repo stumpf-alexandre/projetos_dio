533171eb9e31c4ba5d6a4bb95348c40680148a5a
stumpf-alexandre <als.stumpf@gmail.com>
2022-08-15 12:58:03 -0300

    utilizando variaveis do tipo string no php

---
+++ FormaçãoPHPExperience/tiposDeVariáveis/strings.php
@@ -0,0 +1,28 @@
+<?php
+//é usado com aspas duplas ou aspas simples no php
+//é utilizado para armazenar palavras ou números ou mostrala na função diretamente
+
+    echo "Curso tipos de variáveis em PHP\n";
+
+    $s = "Curso de PHP\n";
+    $valorDia = "80";
+    $gorjeta = "15";
+    $soma = $valorDia + $gorjeta;
+
+    echo $s;
+
+    echo "{$soma} reais\n";
+
+    //texto em h1(html) usado em php
+    $h1 = "<h1>Texto H1</h1>\n";
+    $h2 = "<h2>Texto H2</h2>\n";
+    $h3 = "<h3>Texto H3</h3>\n";
+    $h4 = "<h4>Texto H4</h4>\n";
+    $h5 = "<h5>Texto H5</h5>\n";
+
+    echo $h1;
+    echo $h2;
+    echo $h3;
+    echo $h4;
+    echo $h5;
+?>
