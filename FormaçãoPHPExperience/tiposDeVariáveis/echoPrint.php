f0f1139bf693e02c1a51cf5080cef2313b48bd92
stumpf-alexandre <als.stumpf@gmail.com>
2022-08-15 22:03:16 -0300

    utilizando o print e o echo e suas diferenças

---
+++ FormaçãoPHPExperience/tiposDeVariáveis/echoPrint.php
@@ -0,0 +1,6 @@
+<?php
+//o print não aceita duas strings no mesmo print que da erro
+//ja o echo aceita
+    print "exibir na tela um PRINT\n";
+    echo "exibir na tela um ECHO\n", "Hello World";
+?>
