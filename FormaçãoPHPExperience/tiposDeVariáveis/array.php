dd9b1e8a0d1865271506546ccb01bfcec1c02e42
stumpf-alexandre <als.stumpf@gmail.com>
2022-08-15 21:48:32 -0300

    utilizando array no php

---
+++ FormaçãoPHPExperience/tiposDeVariáveis/array.php
@@ -0,0 +1,10 @@
+<?php
+    $cursos = array("PHP", "JAVA", "REDES", "C", "C++");
+
+    print_r($cursos);
+    print_r("$cursos[2]\n");
+    print_r("$cursos[4]\n");
+    print_r("$cursos[0]\n");
+    print_r("$cursos[3]\n");
+    print_r("$cursos[1]\n");
+?>
