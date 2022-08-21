197f9571492f2fc042f1edbc8788b3c355367362
stumpf-alexandre <als.stumpf@gmail.com>
2022-08-21 15:23:37 -0300

    estrutura condicional if, elseif e else em php

---
+++ FormaçãoPHPExperience/logicosECondicionais/ifElseIfElse.php
@@ -0,0 +1,49 @@
+<?php
+    $nota = 4;
+
+    if ($nota >= 7) {
+        echo "Aluno(a) aprovado !\n";
+    }elseif(($nota < 7) && ($nota >= 4)){
+        echo "Aluno(a) está em recuperação !\n";
+    }elseif (($nota <4) && ($nota >= 2)) {
+        echo "Aluno(a) está em conselho de classe !\n";
+    }else{
+        echo "Aluno(a) reprovado !\n";
+    }
+
+    $nota = 8;
+
+    if ($nota >= 7) {
+        echo "Aluno(a) aprovado !\n";
+    }elseif(($nota < 7) && ($nota >= 4)){
+        echo "Aluno(a) está em recuperação !\n";
+    }elseif (($nota <4) && ($nota >= 2)) {
+        echo "Aluno(a) está em conselho de classe !\n";
+    }else{
+        echo "Aluno(a) reprovado !\n";
+    }
+
+    $nota = 3;
+
+    if ($nota >= 7) {
+        echo "Aluno(a) aprovado !\n";
+    }elseif(($nota < 7) && ($nota >= 4)){
+        echo "Aluno(a) está em recuperação !\n";
+    }elseif (($nota <4) && ($nota >= 2)) {
+        echo "Aluno(a) está em conselho de classe !\n";
+    }else{
+        echo "Aluno(a) reprovado !\n";
+    }
+
+    $nota = 1;
+
+    if ($nota >= 7) {
+        echo "Aluno(a) aprovado !\n";
+    }elseif(($nota < 7) && ($nota >= 4)){
+        echo "Aluno(a) está em recuperação !\n";
+    }elseif (($nota <4) && ($nota >= 2)) {
+        echo "Aluno(a) está em conselho de classe !\n";
+    }else{
+        echo "Aluno(a) reprovado !\n";
+    }
+?>
