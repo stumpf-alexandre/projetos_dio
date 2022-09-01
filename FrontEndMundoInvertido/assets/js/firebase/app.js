a87369a896e8cb261745369383075f3a575e7d48
stumpf-alexandre <als.stumpf@gmail.com>
2022-09-01 18:34:47 -0300

    acrescentando backend para banco de dados firebase e resolvendo bug de imagem com javascript

---
+++ FrontEndMundoInvertido/assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyADZuTdtTEOEd8zS_WA10C8gNXG34cN8KY",
+    authDomain: "projeto-teste-85f9a.firebaseapp.com",
+    projectId: "projeto-teste-85f9a",
+    storageBucket: "projeto-teste-85f9a.appspot.com",
+    messagingSenderId: "1052464531442",
+    appId: "1:1052464531442:web:fd495230998ddec3f3cd6b"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
