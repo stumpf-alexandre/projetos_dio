236205983e6b74bac0311d87d5c810865f391fce
Alexandre Stumpf <als.stumpf@gmail.com>
2022-11-06 17:11:36 -0300

    manipulando multiplas requisições em paraleloe estilizando os carts

--- OrangeTec/pokedex/assets/js/main.js
+++ OrangeTec/pokedex/assets/js/main.js
@@ -1,14 +1,17 @@
+function convertPokemonTypeHtml(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type type${typeSlot.type.name}">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonHtml(pokemon) {
     return `
         <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">#${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    <li class="type typeGrass">Grass</li>
-                    <li class="type typePoison">Poison</li>
+                    ${convertPokemonTypeHtml(pokemon.types).join('')}
                 </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Imagem ${pokemon.name}">
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="Imagem ${pokemon.name}">
             </div>
         </li>
     `
@@ -17,5 +20,6 @@ function convertPokemonHtml(pokemon) {
 const pokemonList = document.getElementById('pokemonList');
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonHtml).join('')
+    const newHtml = pokemons.map(convertPokemonHtml).join('');
+    pokemonList.innerHTML = newHtml;
 })
