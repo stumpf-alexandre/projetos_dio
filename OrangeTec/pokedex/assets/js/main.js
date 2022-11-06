74db9f9e226f6de2437187060c02cb89244f7fa2
Alexandre Stumpf <als.stumpf@gmail.com>
2022-11-06 14:44:49 -0300

    separando o consumo das apis das manipulação do html e diminuindo o código javascript com a função map

--- OrangeTec/pokedex/assets/js/main.js
+++ OrangeTec/pokedex/assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonHtml(pokemon) {
     return `
         <li class="pokemon">
@@ -20,15 +16,6 @@ function convertPokemonHtml(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList');
 
-
-//interface de comunicação de uma promice
-fetch(url)
-    .then((response) => response.json()) //transformando um response em uma promise do body convertido em json
-    .then((jsonBody) => jsonBody.results) //recebe o body convertido e está imprimindo
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonHtml(pokemon);
-        }
-    })
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonHtml).join('')
+})
