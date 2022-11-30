1728628f5540b2ec91e19e538ac4e982271a4bc8
Alexandre Stumpf <als.stumpf@gmail.com>
2022-11-29 21:03:54 -0300

    primeiro projeto de react e deixando bem clean

--- OrangeTec/primeirospassosreact/src/App.js
+++ OrangeTec/primeirospassosreact/src/App.js
@@ -1,7 +1,13 @@
+import Button from "./components/Button";
+
 function App() {
   return (
     <div className="App">
-      Olá react
+      <h1>Olá react</h1>
+      <Button title="Entrar"/>
+      <Button title="Fechar"/>
+      <Button title="Abrir"/>
+      <Button title="Salvar"/>
     </div>
   );
 }
