eba0d305db2b2cfb8642c573f586aea00509f782
Alexandre Stumpf <als.stumpf@gmail.com>
2022-12-10 17:48:51 -0300

    implementando o hook useMemo

--- OrangeTec/projeto-react-cli/src/pages/index.jsx
+++ OrangeTec/projeto-react-cli/src/pages/index.jsx
@@ -1,10 +1,12 @@
 import { 
     useState, 
     useEffect, 
+    useMemo,
 } from 'react';
 
 //useState é um hook utilizamos para lida com um estado dentro de um componente
 //useEffect é um hook que vai trabalhar o ciclo de vida do meu componente, gerencia os efeitos dos componentes
+//useMemo é um hook que memoriza alguma coisa quando meu componente renderiza
 
 const Teste = () => {
     const [name, setName] = useState('Usuario');
@@ -15,8 +17,15 @@ const Teste = () => {
         setName(prev => prev === 'Usuario' ? 'Alexandre' : 'Usuario')
     }
 
+    const calculo = useMemo(() => {
+        alert('calculou')
+        return 10 * 2934947
+    }, []);
+
+    console.log('renderizou', calculo);
+
     useEffect(() => {
-        alert('renderizei')
+        alert('renderizei');
         //implementando aqui voce faz com que o useEffect execute uma vez
 
         //podemos criar um return com função para quando o componente saia da tela ou arvore de componentes
