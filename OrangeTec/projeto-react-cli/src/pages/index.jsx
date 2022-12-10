8805d0f9c3c27d59c4677656758727f6b64c8ae2
Alexandre Stumpf <als.stumpf@gmail.com>
2022-12-10 18:24:06 -0300

    implementando o hook useCallback para funções

--- OrangeTec/projeto-react-cli/src/pages/index.jsx
+++ OrangeTec/projeto-react-cli/src/pages/index.jsx
@@ -2,27 +2,41 @@ import {
     useState, 
     useEffect, 
     useMemo,
+    useCallback,
 } from 'react';
 
 //useState é um hook utilizamos para lida com um estado dentro de um componente
 //useEffect é um hook que vai trabalhar o ciclo de vida do meu componente, gerencia os efeitos dos componentes
 //useMemo é um hook que memoriza alguma coisa quando meu componente renderiza, principalmente para performance do aplicativo que precisa fazer calculos
+//useCallback é um hook muito parecido com o useMemo, mas ele armazena uma função ao invez de um calculo
 
 const Teste = () => {
     const [name, setName] = useState('Usuario');
+    const [age, setAge] = useState(34);
 
-    const handleChangeName = () => {
+    //const handleChangeName = () => {
         //setName('Alexandre')
         //aqui podemos fazer uma validação
-        setName(prev => prev === 'Usuario' ? 'Alexandre' : 'Usuario')
-    }
+        //setName(prev => prev === 'Usuario' ? 'Alexandre' : 'Usuario')
+    //}
 
-    const calculo = useMemo(() => {
-        alert('calculou')
-        return 10 * 2934947
-    }, []);
+    const handleChangeName = useCallback(() => {
+        setName(prev => prev === 'Usuario' ? 'Alexandre' : 'Usuario');
+    },[])
 
-    console.log('renderizou', calculo);
+    const handleChangeAge = useCallback(() => {
+        const newAge = 10 * age;
+        console.log('Idade atual', age, newAge)
+        setAge(prev => prev === 34 ? 43 : 34)
+    },[])//sem arrey de dependencia
+      //[age] com arrey de dependencia
+
+    //const calculo = useMemo(() => {
+        //alert('calculou')
+        //return 10 * 2934947
+    //}, []);
+
+    //console.log('renderizou', calculo);
 
     useEffect(() => {
         alert('renderizei');
@@ -38,9 +52,16 @@ const Teste = () => {
     return(
         <div>
             <p>
-                {name}
+                Nome: {name}
             </p>
-            <button onClick={handleChangeName}>Alterar</button>
+            <br/>
+            <p>
+                Idade: {age}
+            </p>
+            <br/>
+            <button onClick={handleChangeName}>Alterar nome</button>
+            <br/>
+            <button onClick={handleChangeAge}>Alterar idade</button>
         </div>
     )
 }
