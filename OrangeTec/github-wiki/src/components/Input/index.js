6a9db02a393c58c2e292e3d807ecfe68421d681c
Alexandre Stumpf <als.stumpf@gmail.com>
2022-12-27 22:18:46 -0300

    termonando o projeto api com github com a exclusão do repositório com filter

--- OrangeTec/github-wiki/src/components/Input/index.js
+++ OrangeTec/github-wiki/src/components/Input/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { InputContainer } from './styles';
 
-function Input () {
+function Input ({value, onChange}) {
     return (
         <InputContainer>
-            <input />
+            <input value={value} onChange={onChange}/>
         </InputContainer>
     )
 }
 
-export default Input;
+export default Input
---
+++ OrangeTec/github-wiki/src/components/ItemRepo/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+    h3 {
+        font-size: 32px;
+        color: #fafafa;
+    }
+    p {
+        font-size: 16px;
+        color: #fafafa60;
+        margin-bottom: 10px;
+    }
+    a {
+        color: #34d375;
+    }
+    a.remover {
+        color: #ff0000;
+        margin-top: 30px;
+    }
+    hr {
+        color: #fafafa60;
+        margin: 20px 0;
+    }
+`
