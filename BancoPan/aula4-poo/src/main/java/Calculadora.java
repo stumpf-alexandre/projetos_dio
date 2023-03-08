e0dcfeaff4e98c83cdd25c81c203b099b87d34ad
Alexandre Stumpf <als.stumpf@gmail.com>
2023-03-07 22:21:28 -0300

    criando uma interface e sua implementação na classe

---
+++ BancoPan/aula4-poo/src/main/java/Calculadora.java
@@ -0,0 +1,22 @@
+class Calculadora implements OperacaoMatematica {
+
+    @Override
+    public void soma(double operador1, double operador2) {
+        System.out.println("Soma: " + operador1 + operador2);
+    }
+
+    @Override
+    public void subtracao(double operador1, double operador2) {
+        System.out.println("Subtração: " + (operador1 - operador2));
+    }
+
+    @Override
+    public void multiplicacao(double operador1, double operador2) {
+        System.out.println("Multiplicação: " + operador1 * operador2);
+    }
+
+    @Override
+    public void divisao(double operador1, double operador2) {
+        System.out.println("Divisão: " + operador1 / operador2);
+    }
+}
