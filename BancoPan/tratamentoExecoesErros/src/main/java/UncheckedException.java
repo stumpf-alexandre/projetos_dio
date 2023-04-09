47bb953226ddc1a622517ba597da84c801c98896
Alexandre Stumpf <als.stumpf@gmail.com>
2023-04-09 20:54:11 -0300

    criando uma unchecked exception

---
+++ BancoPan/tratamentoExecoesErros/src/main/java/UncheckedException.java
@@ -0,0 +1,41 @@
+package tratamentoExecoesErros.src.main.java;
+
+import javax.swing.JOptionPane;
+
+//fazer a fdivisão de 2 valores inteiros
+public class UncheckedException {
+    public static void main(String[] args) {
+        boolean continueLooping = true;
+
+        do {
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+
+            try {
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + resultado);
+                JOptionPane.showMessageDialog(null, "Resultado: " + resultado);
+                continueLooping = false; // Para sair do looping.
+            } catch (NumberFormatException e) {
+                // Menssagem para o programador.
+                e.printStackTrace();
+                // Menssagem personalizada para o usuário.
+                JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro " + e.getMessage());
+            } catch (ArithmeticException e) {
+                // Menssagem para o programador.
+                e.printStackTrace();
+                // Menssagem personalizada para o usuário.
+                JOptionPane.showMessageDialog(null, "Impossivel dividir um número por zero " + e.getMessage());
+            } finally {
+                System.out.println("Chegou ao finally");
+            }
+        } while (continueLooping);
+
+        System.out.println("O código continua...");
+
+    }
+
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+}
