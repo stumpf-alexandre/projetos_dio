cf108936d695e3f45541167b85116c5678781066
Alexandre <als.stumpf@gmail.com>
2025-04-26 00:58:32 -0300

    criado varios botões com titulos diferentes

--- FormaçãoReactDeveloper/primeiro-app/src/App.js
+++ FormaçãoReactDeveloper/primeiro-app/src/App.js
@@ -1,8 +1,12 @@
-
+import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
-      Ola react
+      <h1>Olá React</h1>
+      <Button>Entrar</Button>
+      <Button>Fechar</Button>
+      <Button>Abrir</Button>
+      <Button>Salvar</Button>
     </div>
   );
 }
