394e1561d126a571530620657f18a5508be41fc3
Alexandre <als.stumpf@gmail.com>
2025-05-03 20:51:30 -0300

    criando funções de soma, divisão, subtração multiplicação e mais

--- FormaçãoReactDeveloper/desafio01-calculadora/src/App.js
+++ FormaçãoReactDeveloper/desafio01-calculadora/src/App.js
@@ -11,7 +11,20 @@ const App = () => {
   const [operation, setOperation] = useState('');
 
   const handleAddNumber = (num) => {
-    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`);
+    if(num === '.') {
+      if(currentNumber.includes('.')) return;
+      if(currentNumber === '0') {
+        setCurrentNumber('0.');
+      } else {
+        setCurrentNumber(currentNumber + '.');
+      }
+    } else {
+      if(currentNumber === '0') {
+        setCurrentNumber(num);
+      } else {
+        setCurrentNumber(currentNumber + num);
+      }
+    }
   };
 
   const handleOnClear = () => {
@@ -27,8 +40,55 @@ const App = () => {
       setOperation('+');
     } else {
       const sum = Number(firstNumber) + Number(currentNumber);
-      setCurrentNumber(String(sum));
+      setCurrentNumber(String(sum.toFixed(2)));
       setOperation('');
+    }
+  }
+
+  const handleMinusNumbers = () => {
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('-');
+    } else {
+      const minus = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(minus.toFixed(2)));
+      setOperation('');
+    }
+  }
+
+  const handleMultiplicationNumbers = () => {
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('x');
+    } else {
+      const mult = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(mult.toFixed(2)));
+      setOperation('');
+    }
+  }
+
+  const handleDivisionNumbers = () => {
+    if(firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('/');
+    } else {
+      const divi = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(divi.toFixed(2)));
+      setOperation('');
+    }
+  }
+
+  const handlePercentNumbers = () => {
+    if(currentNumber !== '0') {
+      const percent = Number(currentNumber) / 100;
+      setCurrentNumber(String(percent.toFixed(2)));
+      handleEquals();
+      setOperation('');
+    } else {
+      setCurrentNumber('0');
     }
   }
 
@@ -38,7 +98,18 @@ const App = () => {
         case '+':
           handleSumNumbers();
           break;
-      
+        case '-':
+          handleMinusNumbers();
+          break;
+        case 'x':
+          handleMultiplicationNumbers();
+          break;
+        case '/':
+          handleDivisionNumbers();
+          break;
+        case '%':
+          handlePercentNumbers();
+          break;
         default:
           break;
       }
@@ -52,26 +123,25 @@ const App = () => {
         <Row>
           <Button label="C" onClick={() => setCurrentNumber('0')}/>
           <Button label="CE" onClick={handleOnClear}/>
-         
-          <Button label="%" onClick={() => handleAddNumber('')}/>
+          <Button label="%" onClick={handlePercentNumbers}/>
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="/" onClick={() => handleAddNumber('')}/>
+          <Button label="/" onClick={handleDivisionNumbers}/>
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="x" onClick={() => handleAddNumber('')}/>
+          <Button label="x" onClick={handleMultiplicationNumbers}/>
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="-" onClick={() => handleAddNumber('')}/>
+          <Button label="-" onClick={handleMinusNumbers}/>
         </Row>
         <Row>
           <Button label="0" onClick={() => handleAddNumber('0')}/>
